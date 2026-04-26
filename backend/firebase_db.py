7db528741cdabec7b204b169597626cb20f52582
Maha Lakshmi <mallimahalakshmi3@gmail.com>
2026-04-26 02:46:41 -0400

    fixed firebase-key

--- backend/firebase_db.py
+++ backend/firebase_db.py
@@ -1,12 +1,17 @@
+import json
+import os
+
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
-import json
 
-# Initialize Firebase
 if not firebase_admin._apps:
-    firebase_key = os.getenv("FIREBASE_KEY")  
-    cred = credentials.Certificate(json.loads(firebase_key))
+    firebase_key = os.getenv("FIREBASE_KEY")
+    
+    if firebase_key:
+        cred = credentials.Certificate(json.loads(firebase_key))
+    else:
+        cred = credentials.Certificate("firebase-key.json")
+    
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
